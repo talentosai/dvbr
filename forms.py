658d0595c25c372a67cc9f551e608be17cc56222
iain <iainbhamilton83@icloud.com>
2025-01-23 11:00:00 +0000

    Updates from brand guides etc

--- forms.py
+++ forms.py
@@ -1,29 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField
-from wtforms.fields.choices import SelectField
-from wtforms.fields.simple import TextAreaField, FileField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.fields.choices import SelectField, RadioField
 from flask_wtf.file import FileField, FileAllowed
-
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
 from wtforms import validators
-import secrets
-
-app = Flask(__name__)
-foo = secrets.token_urlsafe(16)
-app.secret_key = foo
-Bootstrap5(app)
-csrf = CSRFProtect(app)
-
 
 class NewNewsArticleForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(1, 60)])
     content = TextAreaField('Content', [validators.DataRequired(), Length(1, 6000)])
-    image = FileField('Image', validators=[FileAllowed(['jpg', 'png'])])
+    image = RadioField('Image', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Submit')
 
-
 class Login(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 60)])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Length(1, 60),
+        Regexp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', 
+               message="Please enter a valid email address")
+    ])
+    submit = SubmitField('Submit')
+
+class ImageForm(FlaskForm):
+    image = FileField("photo", validators=[FileAllowed(['jpg','jpeg','png'])])
     submit = SubmitField('Submit')
